99cdfb63f1c43cea12bb41ff62bb61ba3b8a7291
Harsh10221 <bot54231h@gmail.com>
2025-12-31 00:46:20 +0530

    First commit

---
+++ src/main/java/com/banking/net_banking_system/NetBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.banking.net_banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/net_banking_system/NetBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.net_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
